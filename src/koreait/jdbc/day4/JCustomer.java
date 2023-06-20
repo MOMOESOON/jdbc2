5b5ea6206eed8f673bfd1f28a199fb9d27f63f63
MOMOESOON <sbag17230@gmail.com>
2023-06-20 13:31:24 +0900

    #5 dto

---
+++ src/koreait/jdbc/day4/JCustomer.java
@@ -0,0 +1,24 @@
+package koreait.jdbc.day4;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Getter
+@ToString
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class JCustomer {		//DTO : JBuy , JProduct
+	
+	private String customid;
+	private String name;
+	private String email;
+	private int age;
+	private Date regDate;
+}
